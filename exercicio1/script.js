0c2a5afd50758be9fb36c850334ef1ed3ae367b2
Marioccarneiro <mario.ccarneiro@hotmail.com>
2023-02-08 22:19:25 -0300

    resolucao-exercicios-strings

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const nomeDeUsuario = prompt(`Qual seu nome?`)
+const comidaFavorita1 = prompt(`Qual sua primeira comida favorita?`)
+const comidaFavorita2 = prompt(`Qual sua segunda comida favorita?`)
+const comidaFavorita3 = prompt(`Qual sua terceira comida favorita?`)
+
+console.log(`Estas sao as comidas favoritas de ${nomeDeUsuario}
+\n comida 1: ${comidaFavorita1}
+\n comida 2: ${comidaFavorita2}
+\n comida 3: ${comidaFavorita3}.`)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,10 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const minhaStringTrimmada = minhaString.trim()
+
+const minhaStringReplace = minhaString.replace(`________` , `sticioso`)
+
+console.log(minhaStringTrimmada)
+console.log(minhaString.length)
+console.log(minhaStringTrimmada.length)
+console.log(minhaStringReplace)
